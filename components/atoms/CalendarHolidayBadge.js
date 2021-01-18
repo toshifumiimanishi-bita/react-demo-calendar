00888e221fb55b1cc7280948195e0ab0e0fad075
toshifumiimanishi-bita <toshifumi_imanishi@bita.jp>
2021-01-18 16:38:15 +0900

    祝日コンポーネントの新規作成

---
+++ components/atoms/CalendarHolidayBadge.js
@@ -0,0 +1,11 @@
+import AppBadge from './AppBadge'
+
+const CalendarHolidayBadge = () => {
+  return (
+    <AppBadge className="bg-yellow-500 text-white">
+      スポーツの日
+    </AppBadge>
+  )
+}
+
+export default CalendarHolidayBadge 
